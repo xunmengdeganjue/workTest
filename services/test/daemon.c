605749017a984308f32f7755cd15d7783d2a4248
xunmengdeganjue <542873805@qq.com>
2015-12-29 06:27:39 -0800

    update the daemon.c 增加信号处理，及PID文件处理

--- services/test/daemon.c
+++ services/test/daemon.c
@@ -4,24 +4,52 @@
 #include<signal.h>
 #include<string.h>
 
+#include<fcntl.h>
 
 
 //#define PIDFILE "/var/run/ip6scan.pid"
 
 #define PIDFILE "/tmp/ip6scan.pid"
+
+char *runfile = NULL;
+
 static void recordPid(){
+	
 	FILE *fp;
+	int fd;
+	
 	char buffer[256] = {0};
-	sprintf(buffer,"%d\n",getpid());
+	struct flock lock;
 	
-	if((fp = fopen(PIDFILE,"w")) == NULL){
-		printf("open the file failed\n");
-		exit(1);
+	lock.l_type = F_WRLCK;
+	lock.l_whence = SEEK_SET;
+	
+	runfile = PIDFILE;
+	if(runfile){
+		
+		//unlink(runfile);
+		
+		if((fp = fopen(PIDFILE,"w")) == NULL){
+			printf("open the file failed\n");
+			exit(1);
+		}
+		
+		fd = fileno(fp);
+		
+		if( fcntl(fd, F_SETLK, &lock) < 0 ){
+			printf("set lock to the pidfile %s failed!\n",runfile);
+			unlink(runfile);
+		}
+		
+		sprintf(buffer,"%d\n",getpid());	
+		
+		printf("the pid is %s \n",buffer);
+		fwrite(buffer,sizeof(buffer),1,fp);
+		fclose(fp);
 	}
-	printf("the pid is %s \n",buffer);
-	fwrite(buffer,sizeof(buffer),1,fp);
 	
-	fclose(fp);
+	
+	
 	
 }
 
@@ -33,7 +61,13 @@ static void sigUsr(int signo){/*argument is signal number*/
 	if (signo == SIGUSR1){
 		printf("received SIGUSR1 [%d]\n",signo);
 		dataTransmission();
-	}else{
+	}else if(signo == SIGTERM){
+		printf("USER STOP THE DAEMON\n");
+		remove(runfile);
+	}else if(signo == SIGKILL){
+		printf("USER IS SEND A KILL SIGNAL\n");	
+	}
+	else{
 		printf("received signal %d \n",signo);
 	}
 }
@@ -41,8 +75,6 @@ static void sigUsr(int signo){/*argument is signal number*/
 int main(int argc,char **argv){
 
 	printf("---------hello world\n");
-	
-	recordPid();
 	
 	if(signal(SIGUSR1,sigUsr) == SIG_ERR){
 			printf("can't catch SIGUSR1\n");
@@ -54,15 +86,14 @@ int main(int argc,char **argv){
 	{
 		printf("loop\n");
 		
+		recordPid();
+		
 		sleep(5);
 
 		pause();//let the program 
 		
 	}
-
-
-
 	
-
-
+	remove(PIDFILE);
+	
 }
