bd60c1fed6517d5cca21883d19fdcb570d0e8e70
xunmengdeganjue <542873805@qq.com>
2016-01-06 14:27:10 +0800

    add the logrecord for optimaize the daemon's log system

---
+++ services/test/logrecord/daemon_log.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+
+int daemon_log(const char *format,...);
+
+
---
+++ services/test/logrecord/main.c
@@ -0,0 +1,16 @@
+#include "daemon_log.h"
+
+int main(){
+	
+	printf("hello how are you?\n");
+	
+	logtest();
+	
+	printf("reach the end point!\n");
+	
+	
+	return 0;
+
+}
+
+
