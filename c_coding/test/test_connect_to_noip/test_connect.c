1ea9574023336f66571e53bf2c9746ae974e9663
xunmengdeganjue <542873805@qq.com>
2016-08-10 15:59:24 +0800

    nick:update the test code

--- c_coding/test/test_connect_to_noip/test_connect.c
+++ c_coding/test/test_connect_to_noip/test_connect.c
@@ -12,14 +12,14 @@
 /* $$.If bzero$$ */
 /* $$.If memset$$ */
 #endif
+#define PORT 8245 	/*noip server port*/
+#define HOST_IP "8.23.224.120"
 
 int main(int argc,char **argv){
 	int sockfd,n;
 	char recvline[MAXLINE + 1];
 	struct sockaddr_in servaddr;
 	
-	if(argc != 2)
-		printf("usage:a.out <IPaddress>");
 
 	if((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0)
 		//if((sockfd = socket(9999,SOCK_STREAM,0)) < 0)
@@ -28,14 +28,18 @@ int main(int argc,char **argv){
 
 	bzero(&servaddr,sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port   = htons(8245); /*noip server port*/
-	if(inet_pton(AF_INET,argv[1],&servaddr.sin_addr) <= 0)
-		printf("inet_pton error for %s", argv[1]);
+	servaddr.sin_port   = htons(PORT); 
+	
+	if(inet_pton(AF_INET,HOST_IP,&servaddr.sin_addr) <= 0)
+		printf("inet_pton error for %s", HOST_IP);
 
 	if(connect(sockfd,(SA *)&servaddr,sizeof(servaddr)) < 0){
 		printf("connect error\n");
 		exit(1);
+	}else{
+		printf("Connect successfully!\n");	
 	}
+	
 	while((n = read(sockfd,recvline,MAXLINE)) > 0){
      
 		recvline[n] = 0; /*null terminate*/
