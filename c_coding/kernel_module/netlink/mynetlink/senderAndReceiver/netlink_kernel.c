b84d199c36c4f805127c74f8adc8a9ec78573ee4
xunmengdeganjue <542873805@qq.com>
2018-12-20 18:02:24 +0800

    update

--- c_coding/kernel_module/netlink/mynetlink/senderAndReceiver/netlink_kernel.c
+++ c_coding/kernel_module/netlink/mynetlink/senderAndReceiver/netlink_kernel.c
@@ -6,7 +6,6 @@ space-via-netlink-in-c
 	http://www.linuxjournal.com/node/7356/print
 	
 */
-
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/types.h>
@@ -15,66 +14,55 @@ space-via-netlink-in-c
 #include <linux/netlink.h>
 #include <linux/skbuff.h>
 
-//#include <linux/init.h>
-//#include <linux/ip.h>
-
-
 #ifndef NETLINK_EXAMPLE
 #define NETLINK_EXAMPLE 17
 #endif
 
 struct sock *nl_sk = NULL;
 #define MYMGRP 1
+enum {
+	UNICAST,
+	BROADCAST,
+	MULTICAST
+};
 
-
-#if 0
-static void nl_data_ready(struct sk_buff *__skb)
-{
-    struct sk_buff *skb;
-    struct nlmsghdr *nlh = NULL;
-	while((skb = skb_get(__skb)) != NULL) {
-		nlh = (struct nlmsghdr *)skb->data;
-		//nlh = nlmsg_hdr(skb);
-		printk("%s: received netlink message payload: %s \n", __FUNCTION__, (char*)NLMSG_DATA(nlh) );
-		kfree_skb(skb);
-		break;
-    }
-    printk("recvied finished!\n");
-}
-#endif
-
-
-void parse_message(struct sk_buff *__skb, char *message){
+/*
+@name:parse_message
+@parm:
+	__skb:The socket buffer.
+@description:
+	parse the message received from the user space.
+*/
+void parse_message(struct sk_buff *__skb){
 
 	struct sk_buff *skb;
 	struct nlmsghdr *nlh = NULL;
-	while((skb = skb_get(__skb)) != NULL) {
+	char string[128] = {0};
+	
+	while((skb = skb_get(__skb)) != NULL) {/*skb_get对套接字缓冲区再引用一次，返回指向缓冲区的指针*/
 	   	nlh = (struct nlmsghdr *)skb->data;
-	   	//nlh = nlmsg_hdr(skb);
-	   	printk("%s: received netlink message payload: %s \n", __FUNCTION__, (char*)NLMSG_DATA(nlh) );
-		//strcpy(message, (char*)NLMSG_DATA(nlh));
-	   	kfree_skb(skb);
+		printk("%s: received netlink message payload: %s \n", __FUNCTION__, (char*)NLMSG_DATA(nlh) );
+
+		/*Following code is the other way deal with the data.*/
+		//nlh = nlmsg_hdr(skb);
+		//memcpy(string, NLMSG_DATA(nlh), sizeof(string));
+	   	//printk("%s:received netlink message payload: %s\n", __FUNCTION__, string);
+		
+	   	kfree_skb(skb); /*删除对一个缓冲区的引用*/
 	   	break;
 	}
 	printk("recvied finished!\n");
 }
 
-
-void send_to_user(struct sk_buff *__skb) 
-{
-    struct sk_buff *skb = NULL;
+int nl_msg_send(char *message, int send_type){
+	struct sk_buff *skb = NULL;
     struct nlmsghdr *nlh;
 	int ret = 0;
-	char *message = "Hello from the kernel!";
-
-	int msg_size = strlen(message) + 1;
-
-	printk("Eneter the send_to_user!\n");
-
-	
+	int pid = 0;
+	int msg_size = strlen(message);
 	
 	/*开辟一个新的套接字缓存*/
-	skb = nlmsg_new(NLMSG_ALIGN(msg_size + 1), GFP_KERNEL);	
+	skb = nlmsg_new(NLMSG_ALIGN(msg_size), GFP_KERNEL); 
 	//skb = nlmsg_new(NLMSG_DEFAULT_SIZE, GFP_KERNEL);
 	if(!skb){
 		printk("Allocation failure!\n");
@@ -85,21 +73,46 @@ void send_to_user(struct sk_buff *__skb)
 	strcpy(NLMSG_DATA(nlh), message);
 
 	printk("send skb message!\n");
-
-	/*multicast the message to all listening processes*/
-	//ret = netlink_broadcast(nl_sk, skb, 0, 1, GFP_KERNEL);
-	ret = nlmsg_multicast(nl_sk, skb, 0, MYMGRP, GFP_KERNEL);
-	//ret = netlink_unicast(nl_sk, skb, pid, MSG_DONTWAIT);
+	switch(send_type){
+	case UNICAST:
+		printk("send data by unicast!\n");
+		ret = netlink_unicast(nl_sk, skb, pid, MSG_DONTWAIT);
+		break;
+	case BROADCAST:
+		printk("send data by broadcast!\n");
+		ret = netlink_broadcast(nl_sk, skb, 0, 1, GFP_KERNEL);
+		break;
+	case MULTICAST:
+	default:
+		printk("send data by multicast!\n");
+		ret = nlmsg_multicast(nl_sk, skb, 0, MYMGRP, GFP_KERNEL);
+		break;
+	}
+	
 	if(ret < 0){
 		printk("message send error! [%d]\n",ret);
 	}else{
 		printk("Send successfully! The data is:[%s]\n", NLMSG_DATA(nlh));
-		
 	}
+	return ret;
+
+}
+
+void data_operation(struct sk_buff *__skb) 
+{
+
+	char *message = "Hello from the kernel!";
+	printk("Eneter the %s!\n",__FUNCTION__);
+
+	/*parse the data that comes from the user-space*/
+	parse_message(__skb);
+	/*send the data from the kernel space to the user-space*/
+	nl_msg_send(message,MULTICAST);
+	
 }
 
 struct netlink_kernel_cfg cfg = {
-    .input = send_to_user,
+    .input = data_operation,
 };
 
 static int __init  testnetlink_init(void)
@@ -115,15 +128,24 @@ static void __exit  testnetlink_exit(void)
 {
 	printk(KERN_INFO "remove the kernel module netlink_kernel_module.ko !\n");
 	netlink_kernel_release(nl_sk);
+	nl_sk = NULL;
 }
 
 module_init(testnetlink_init);
 module_exit(testnetlink_exit);
 
-
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("nickli");
 MODULE_DESCRIPTION("MY Netlink Test Kernel Module");
+
+void send_data_to_user(char *msg,int len){
+
+	if(msg != NULL && len != 0){
+		nl_msg_send(msg,BROADCAST);
+	}
+
+}
+EXPORT_SYMBOL(send_data_to_user);
 
 
 
